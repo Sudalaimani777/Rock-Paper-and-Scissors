2d17250acfc7823a6def60893d344c6b2deab4ef
K.Sudalaimani <sudalaimani5112007@gmail.com>
2025-06-16 10:02:18 +0530

    Add files via upload

---
+++ logic.js.js
@@ -0,0 +1,96 @@
+// const btnOne = document.getElementById("btnOne");
+
+
+// const purchased = [
+//     {
+//         id: 1,
+//         item : "Shirts"
+//     },
+
+//     {
+//         id : 2,
+//         item : "Pants"
+//     },
+
+//     {
+//         id
+//     }
+
+// ]
+
+
+// console.log(purchased);
+
+// //Adding Delete Event 
+// btnOne.addEventListener("click", e => {
+    
+//     delete purchased[0].item;
+//     console.log(purchased[0]);
+
+// })
+
+
+const playerChoice = prompt(`Enter the rock, paper or scissors`).toLocaleLowerCase().trim();
+
+// console.log(playerChoice);
+
+const  randomNumber = Math.ceil(Math.random() * 3);
+console.log(randomNumber);
+
+
+let computer = "";
+
+switch (randomNumber) {
+    case 1:
+        computer = "rock";
+        break;
+    case 2 :
+        computer = "paper";
+        break;
+    case 3 :
+        computer = "scissors";
+        break;
+    default:
+        computer = `Choose any`;
+        break;
+}
+
+// console.log(computer);
+
+if (playerChoice === computer) {
+    alert(`The game is draw because you choose ${playerChoice} and the computer choosed ${computer}`)
+}
+
+else if (playerChoice === "rock" && computer === "paper"){
+   alert(`Computer won because the computer chooses ${computer}`);
+}
+
+else if (playerChoice === "rock" && computer === "scissors") {
+    alert (`You won because the computer chooses ${computer}`);
+}
+
+else if (playerChoice === "paper" && computer === "rock"){
+     alert (`You won because the computer choosed ${computer}`);
+}
+
+else if (playerChoice === "paper" && computer === "scissors"){
+     alert (`Computer won because the computer choosed ${computer}`);
+}
+
+else if (playerChoice === "scissors" && computer === "paper"){
+     alert (`You won because the computer choosed ${computer}`);
+}
+
+else if (playerChoice === "scissors" && computer === "rock"){
+     alert (`Computer won because the computer choosed ${computer}`);
+}
+
+else {
+    alert(`Please choose any objects like rock, paper or scissors`)
+}
+
+
+
+
+    
+    
